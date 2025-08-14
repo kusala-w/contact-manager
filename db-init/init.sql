b576a671576576844def7743044d5ce85834ddab
Kus <kusala@gmail.com>
2025-08-14 17:23:10 -0400

    Added drop statements + moved the FK reference inline

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS contact_history;
+DROP TABLE IF EXISTS contact;
+
 CREATE TABLE contact(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
@@ -5,14 +8,13 @@ CREATE TABLE contact(
     email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(255) NOT NULL,
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE
-)
+);
 
 CREATE TABLE contact_history(
     id SERIAL PRIMARY KEY,
-    contact_id INT NOT NULL,
+    contact_id INT NOT NULL REFERENCES contact(id),
     action VARCHAR(255) NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (contact_id) REFERENCES contact(id)
-)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
+);
 
