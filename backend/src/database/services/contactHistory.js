981de8144049e6c1ec8ae4df200bbd385c0c9542
Kus <kusala@gmail.com>
2025-08-18 11:37:33 -0400

    Added the find method

--- backend/src/database/services/contactHistory.js
+++ backend/src/database/services/contactHistory.js
@@ -34,6 +34,25 @@ async function create(history, client) {
     }
 }
 
+async function find(params) {
+    const model = contactHistoryModel.mapToModel(params)
+    const paramsToFilter = _.pickBy(model, value => !_.isUndefined(value))
+
+    const conditions = _.map(paramsToFilter, (value, key) => `${key} = '${value}'`)
+
+    let query = `SELECT * FROM contact`
+    query = conditions.length ? `${query} WHERE ${conditions.join(' AND ')}` : query
+
+    try {
+        const result = await connectionPool.query(query)
+        return result.rows.map(contactHistoryModel.mapToObject)
+    } catch (error) {
+        console.error(`Error in contactHistory.find() for params ${params.join(',')}. Error: ${error}`)
+        throw new DatabaseError()
+    }
+}
+
 module.exports = {
-    create
+    create,
+    find
 }
