31023038606bd033fd46dac22e0bd2ca006c7446
Kus <kusala@gmail.com>
2025-08-18 11:37:54 -0400

    Added update and delete methods

--- backend/src/database/services/contact.js
+++ backend/src/database/services/contact.js
@@ -9,7 +9,7 @@ const contactHistoryService = require('./contactHistory')
 
 async function findAll() {
     try {
-        const result = await connectionPool.query('SELECT * FROM contact')        
+        const result = await connectionPool.query('SELECT * FROM contact')
         return result.rows.map(contactModel.mapToObject)
     } catch(error) {
         console.log(`Error in contact.findAll(). Error: ${error}`)
@@ -31,14 +31,10 @@ async function find(params) {
     const model = contactModel.mapToModel(params)
     const paramsToFilter = _.pickBy(model, value => !_.isUndefined(value))
 
-    const condition = null
-    _.forEach(paramsToFilter, (value, key) => {
-        condition = condition ? `${condition} AND` : ''
-        condition = `${condition} ${key} = ${value}`
-    })
-
+    const conditions = _.map(paramsToFilter, (value, key) => `${key} = '${value}'`)
+    
     let query = `SELECT * FROM contact`
-    query = condition ? `${query} ${condition}` : query
+    query = conditions.length ? `${query} WHERE ${conditions.join(' AND ')}` : query
 
     try {
         const result = await connectionPool.query(query)
@@ -93,9 +89,74 @@ async function create(contact) {
     
 }
 
-async function update(id, contact) {}
+async function update(id, contact) {
+    const model = contactModel.mapToModel(contact)
 
-async function _delete(id) {}
+    const updateFields = []
+
+    _.forEach(model, (value, key) => {
+        if (key === 'id') return
+        updateFields.push(`${key} = ${value}`)
+    })
+
+    const query = `UPDATE contact SET ${updateFields.join(', ')} WHERE id = ${id}`
+
+    const dbClient = await connectionPool.connect()
+
+    try {
+        await dbClient.query('BEGIN')
+        const result = await dbClient.query(query)
+
+        console.log('Update result = ', result)
+
+        await contactHistoryService.create({
+            contactId: id,
+            action: 'Update',
+            description: 'Updated through the app'            
+        }, dbClient)
+
+        await dbClient.query('COMMIT')
+
+        return true
+    } catch (error) {
+        await dbClient.query('ROLLBACK')
+
+        console.error(`Error in contact.update() for contactId '${id}'. Error: ${error}`)
+        throw new DatabaseError()
+    } finally {
+        dbClient.release()
+    }
+}
+
+async function _delete(id) {
+    const query = `UPDATE contact SET is_deleted = true WHERE id = ${id}`
+
+    const dbClient = await connectionPool.connect()
+
+    try {
+        await dbClient.query('BEGIN')
+        const result = await dbClient.query(query)
+
+        console.log('Delete result = ', result)
+
+        await contactHistoryService.create({
+            contactId: id,
+            action: 'Delete',
+            description: 'Deleted through the app'
+        }, dbClient)
+
+        await dbClient.query('COMMIT')
+
+        return true
+    } catch (error) {
+        await dbClient.query('ROLLBACK')
+
+        console.error(`Error in contact.delete() for contactId '${id}'. Error: ${error}`)
+        throw new DatabaseError()
+    } finally {
+        dbClient.release()
+    }
+}
 
 module.exports = {
     findAll,
